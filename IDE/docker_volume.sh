4687b34bdcd4b4b2b4f6c50a82774478a041322c
kschoi728 <kschoi728@daum.net>
2024-11-02 15:35:15 +0900

    third commit

--- IDE/docker_volume.sh
+++ IDE/docker_volume.sh
@@ -1,14 +1,14 @@
-# 볼륨 새성 (volume name : cloud-cicd-src)
+# 볼륨 생성 (volume name : cloud-cicd-src)
 docker volume create  \
---opt device="/Users/dangtongbyun/Library/CloudStorage/GoogleDrive-dbyun@redhat.com/내 드라이브/05.Lecture/cicd/cloud-cicd/src" \
+--opt device="C:\CICD\cicd-start\IDE\src" \
 --opt o=bind --opt type=none cloud-cicd-src
 
 docker volume create  \
---opt device="/Users/dangtongbyun/Library/CloudStorage/GoogleDrive-dbyun@redhat.com/내 드라이브/05.Lecture/cicd/cloud-cicd/vscode" \
+--opt device="C:\CICD\cicd-start\IDE\src" \
 --opt o=bind --opt type=none vscode
 
 docker volume create  \
---opt device="/Users/dangtongbyun/Library/CloudStorage/GoogleDrive-dbyun@redhat.com/내 드라이브/05.Lecture/cicd/cloud-cicd/jenkins" \
+--opt device="C:\CICD\cicd-start\IDE\src" \
 --opt o=bind --opt type=none jenkins-home
 
 # 볼륨 생성 확인
